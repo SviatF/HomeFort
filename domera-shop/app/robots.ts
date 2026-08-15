d318d765e0e194017d72793344f9e99f33aee3a2
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-08-15 15:07:47 -0700

    Delete domera-shop directory

--- domera-shop/app/robots.ts
+++
@@ -1,2 +0,0 @@
-import type { MetadataRoute } from 'next';
-export default function robots(): MetadataRoute.Robots { return { rules: { userAgent:'*', allow:'/', disallow:['/admin/','/checkout/'] }, sitemap:'https://domera.shop/sitemap.xml', host:'https://domera.shop' }; }
