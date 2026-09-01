ff8726649fa0598ee82bfaf8da79a10459c1bb4a
Sviat Petryshyn <85065470+SviatF@users.noreply.github.com>
2026-09-01 04:27:27 -0700

    Add GTM WRXMB8X5 globally

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import '@/ui-a-f.css';
 import AppProviders from '@/components/AppProviders';
 
 const base = process.env.NEXT_PUBLIC_SITE_URL || 'https://domera.shop';
+const GTM_ID = 'GTM-WRXMB8X5';
 
 export const metadata = {
   metadataBase: new URL(base),
@@ -34,7 +35,29 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="uk-UA">
-      <body><AppProviders>{children}</AppProviders></body>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <AppProviders>{children}</AppProviders>
+      </body>
     </html>
   );
 }
